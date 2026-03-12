f684b24b3b534a00b0c6810adb165be5c2098b0e
Xotabich <mitanoow@gmail.com>
2026-03-12 22:10:19 +0100

    the god like jdh pallete

--- src/main/kernel.c
+++ src/main/kernel.c
@@ -1,20 +1,11 @@
 #include "vga.h"
 
 void kernel_main(void) {
-
     vga_generate_pallete();
-    vga_cls(0);
-
-    vga_print(0, 0, "Hello, world!", 255);
-
-    int x0 = 219, y0 = 99;
-    int rect_width = 100;
-    int rect_height = 100;
-
-    for (int y = y0; y < 200; y++) {
-        for (int x = x0; x < 320; x++) {
-            int color_index = ((y - y0) * rect_width + x) * 256 / (rect_width * rect_height);
-            vga_pp(x, y, color_index);
+    for (int y = 0; y < 200; y++) {
+        unsigned char color = y * 255 / 199;
+        for (int x = 0; x < 320; x++) {
+            vga_pp(x, y, color);
         }
     }
 
